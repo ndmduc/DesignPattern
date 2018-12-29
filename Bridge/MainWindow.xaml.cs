55deebf32246dea722f926edbb42526e8b84061b
ducdmn <ndminhduc@gmail.com>
2018-12-29 11:55:11 +0700

    minor update

--- Bridge/MainWindow.xaml.cs
+++ Bridge/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Bridge
             InitializeComponent();
             this.Height = 950;
             this.Width = 925;
-            this.GuiUC.DefineText = "Bridge is used when we need to decouple an abstraction from its implementation so that the two can vary independently.";
+            this.GuiUC.DefineText = "Bridge is used when we need to decouple an abstraction from its implementation so that the two can vary independently. avoid a permanent binding between an abstraction and its implementation. This might be the case, for example, when the implementation must be selected or switched at run - time";
 
             Shape redCircle = new Circle(10, 10, 10, new RedCircle());
             var redDrawed = redCircle.Draw();
@@ -35,6 +35,14 @@ namespace Bridge
 
             var result = redDrawed + "\n" + greenDrawed;
             this.GuiUC.TextBlockText = result;
+
+            this.GuiUC.TextBlockConsequence = @" Decoupling interface and implementation
+• An implementation is not bound permanently to an interface. The implementation of an abstraction can be configured at run-time. It's even possible for an object to change its implementation at run-time
+• Decoupling Abstraction and Implementor also eliminates compile-time dependencies on the implementation. Changing an implementation class doesn't require recompiling the Abstraction class and its clients. This property is essential when you must ensure binary compatibility between different versions of a class library
+ Improved extensibility
+• You can extend the Abstraction and Implementor hierarchies independently
+ Hiding implementation details from clients
+• You can shield clients from implementation details, like the sharing of Implementor objects and the accompanying reference count mechanism (if any)";
         }
     }
 }
--- Decorator/MainWindow.xaml.cs
+++ Decorator/MainWindow.xaml.cs
@@ -13,8 +13,8 @@ namespace Decorator
         {
             InitializeComponent();
 
-            this.Height = 650;
-            this.Width = 725;
+            this.Height = 950;
+            this.Width = 925;
             this.GuiUC.DefineText = "Decorator pattern allows a user to add new functionality to an existing object without altering its structure. This pattern creates a decorator class which wraps the original class and provides additional functionality keeping class methods signature intact.";
 
             IShape circle = new Circle();
@@ -33,6 +33,19 @@ namespace Decorator
             stringBuilder.Append(redRectangle.Draw());
 
             this.GuiUC.TextBlockText = stringBuilder.ToString();
+
+            this.GuiUC.TextBlockConsequence = @"Applicability
+ To add responsibilities to individual objects dynamically and transparently, that is, without affecting other objects for responsibilities that can be withdrawn
+ When extension by sub-classing is impractical. Sometimes a large number of independent extensions are possible and would produce an explosion of subclasses to support every combination. Or a class definition may be hidden or otherwise unavailable for sub-classing
+ Consequences
+ More flexibility than static inheritance
+• Provides a more flexible way to add responsibilities to objects than can be had with static (multiple) inheritance. With decorators responsibilities can be added and removed at runtime simply by attaching and detaching them
+ Avoids feature-laden classes high up in the hierarchy
+• Offers a pay-as-you-go approach to adding responsibilities. Instead of trying to support all foreseeable features in a complex, customizable class, you can define a simple class and add functionality incrementally with Decorator objects. Functionality can be composed from simple pieces. As a result, an application needn't pay for features it doesn't use
+ A decorator and its component aren't identical
+• Acts as a transparent enclosure. But from an object identity point of view, a decorated component is not identical to the component itself. Hence you shouldn't rely on object identity when you use decorators
+ Lots of little objects
+• A design that uses Decorator often results in systems composed of lots of little objects that all look alike. The objects differ only in the way they are interconnected, not in their class or in the value of their variables. Although these systems are easy to customize by those who understand them, they can be hard to learn and debug";
         }
     }
 }
